ff35aba98ec6f284819616df0155e1ee10929b66
Jeff Horsch <jhorsch@gmail.com>
2014-01-31 14:37:36 -0600

    signout working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,9 @@ end
 
 def create
    @user = User.new(user_params)
+
     if @user.save
+        sign_in @user
         flash[:success] = "Welcome to Mail Chimp!"
         redirect_to @user
      else
