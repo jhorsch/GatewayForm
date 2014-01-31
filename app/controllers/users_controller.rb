8ae317d9f6e16e945724d7bb47846d451ae576bd
Jeff Horsch <jhorsch@gmail.com>
2014-01-31 14:25:34 -0600

    working signup/signin

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+
+  # before_action :set_user, only: [show,update,destory,edit]
+
+def index
+
+end
+
+
+
+  def new
+    @user = User.new
+  end
+
+
+def create
+   @user = User.new(user_params)
+    if @user.save
+        flash[:success] = "Welcome to Mail Chimp!"
+        redirect_to @user
+     else
+      render 'new'
+    end
+end
+
+
+def show
+    @user = User.find(params[:id])
+end
+
+
+ private
+    def user_params
+        params.require(:user).permit(:name, :email,:password, :password_confirmation)
+
+    end
+
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GatewayForm::Application.config.session_store :cookie_store, key: '_gatewayForm_session'
