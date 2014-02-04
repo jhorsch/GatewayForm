393e3cff33edc04a4c0920026523d8e8bbfd52d3
Jeff Horsch <jhorsch@gmail.com>
2014-02-04 12:14:11 -0600

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140202233457) do
+ActiveRecord::Schema.define(version: 20140204181336) do
 
   create_table "users", force: true do |t|
     t.string   "email"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140202233457) do
     t.string   "user_name"
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
+    t.integer  "age"
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
